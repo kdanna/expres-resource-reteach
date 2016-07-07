3537ecaad0e2c3fdcac9b724547f1b05c7bf2a8e
kdanna <kayce.danna@gmail.com>
2016-07-06 22:19:13 -0600

    working through the Candies lab

--- starter_code/server.js
+++ starter_code/server.js
@@ -1,6 +1,11 @@
 'use strict'
-var express = require('express')
-var app = express() 
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+var nextId = 1;
 
 var candies = [
   {
@@ -19,6 +24,56 @@ var candies = [
     "color": "Pink"
   }
 ];
+
+//WORKS - "Candies Show Route" - client is requesting information on the whole candies object
+app.get('/candies', function(req, res){
+    res.send(candies);
+});
+
+//WORKS - "Candies Index Route". Will iterate through the whole candies array and if the index has an id that was asked for via search params, then it will respond with that object of candies.
+app.get('/candies/:id', function(req, res){
+  var foundCandy;
+  var targetId = parseInt(req.params.id);
+    for(var i = 0; i<candies.length; i++){
+      if(candies[i].id === targetId){
+        foundCandy = candies[i];
+      }
+    }
+    res.send(foundCandy);
+});
+
+
+//WORKS - "Candies Create Route Id included" - will post a new candy to the array by pushing it with an id it assigns next in order.
+app.post('/candies/:id', function(req, res){
+  var newCandy = req.params.body;
+  if(newCandy.id === undefinded){
+    newCandy.id = nextId;
+    nextId++;
+  }
+  candies.push(newCandy);
+  res.send(newCandy);
+});
+
+
+
+//WORKS - "Candies Create Route ID not Included" - will post a new candy to the object w/o id.
+app.post('/candies', function(req, res){
+    res.send(candies);
+});
+
+//  "delete route"
+app.delete('/candies/:id', function(req, res){
+
+});
+
+//Might be working - "Candies Update Route" I don't see the id show up in Postman, but i do see the color red
+app.put('/candies/:id', function(req, res) {
+  var candyUpdate = req.body
+  res.send(candyUpdate);
+
+});
+
+
 
 app.listen(3000)
 console.log("Server listening on port 3000")
